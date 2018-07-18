159d29406ecda9964353773f0782d84c280b1c7e
Yanl05 <756593069@qq.com>
2018-07-18 21:28:40 +0800

    Create twenty_three.py

---
+++ twenty_three.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        res = []
+        for i in lists:
+            while i:
+                res.append(i.val)
+                i = i.next
+        if res == []:
+            return []
+        print(res)
+        res.sort()
+        print(res)
+        l = ListNode(0)
+        first = l
+        while res:
+            l.next = ListNode(res.pop(0))
+            l = l.next
+        return first.next
