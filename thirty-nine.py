a176df068a05defc0199a2ccd38f53633c17d69a
Yanl05 <756593069@qq.com>
2018-09-21 09:31:11 +0800

    Create thirty-nine.py

---
+++ thirty-nine.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates = list(set(candidates))  # set 生成无序不重复元素集
+        sorted(candidates)  # 原地排序
+        self.result = []
+        start = 0
+        self.backtrack(candidates, target, start, [])
+        return self.result
+    def backtrack(self, candidates, target, start, val):
+        if target == 0:
+            self.result.append(val[:])
+        for i in range(start, len(candidates)):
+            if target > 0:
+                val.append(candidates[i])
+            else:
+                break
+            self.backtrack(candidates, target - candidates[i], i, val)
+            val.pop()
