0199eb899305b60440f1ea9ea768c69121cb723d
Yanl05 <756593069@qq.com>
2018-09-26 08:05:45 +0800

    Create forty-two.py

---
+++ forty-two.py
@@ -0,0 +1,22 @@
+class Solution:
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if not height:
+            return 0
+        n = len(height)
+        ret = 0
+        left_max, right_max = [0] * n, [0] * n
+        left_max[0] = height[0]
+        for i in range(1, n):
+            left_max[i] = max(height[i], left_max[i - 1])
+        right_max[n - 1] = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            right_max[i] = max(height[i], right_max[i + 1])
+
+        for i in range(1, n - 1):
+            tmp = min(left_max[i], right_max[i]) - height[i]
+            ret += tmp
+        return ret
