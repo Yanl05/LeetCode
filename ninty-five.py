4b1c13ebff15a21d175a2879c695f123fd0b46ea
yanl <756593069@qq.com>
2019-05-29 11:13:18 +0800

    DFS

---
+++ ninty-five.py
@@ -0,0 +1,46 @@
+# -*- coding: UTF-8 -*-
+
+"""
+# @Time    : 2019-05-29 10:33
+# @Author  : yanlei
+# @FileName: ninty-five.py
+
+method: DFS
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def help(self, start, end):
+        if start > end:
+            return [None]
+
+        res = []
+        for curr_root in range(start, end + 1):
+            left = self.help(start, curr_root - 1)
+            right = self.help(curr_root + 1, end)
+
+            for l in left:
+                for r in right:
+                    root = TreeNode(curr_root)
+                    root.left = l
+                    root.right = r
+                    res.append(root)
+        return res
+
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        if n == 0:
+            return []
+        return self.help(1, n)
+print(Solution().generateTrees(3))
